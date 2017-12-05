69623afbeae29ccc18201546e627397c141dcd80
Unknown <mohamad222222222@gmail.com>
2017-12-05 18:17:53 +0100

    Bessere Losung

--- Aufgabe 4/Spreadsheet.java
+++ Aufgabe 4/Spreadsheet.java
@@ -5,51 +5,52 @@ import java.util.Arrays;
 public class Spreadsheet {
 	int[][] a;
 	int columns, rows, scellnumer;
+	//sumcells is an arraylist that contains all sumcells with their range
 	ArrayList<int[]> sumcells;
 
 	Spreadsheet(int columns, int rows) {
 		this.columns = columns;
 		this.rows = rows;
 		this.a = new int[rows][columns];
-		this.scellnumer = 0;
 		this.sumcells = new ArrayList<int[]>();
-
 	}
 
+	/*
+	   requiers column>0 &&row>0 
+	   modifies this.a[row-1][column-1] 
+	   ensures this.a[row-1][column-1]=value;
+	           for all sumcells if this.a[row-1][column-1]
+	           between range of a sumcell
+	           this.sumcell=sumoldvalue-oldvalue+value;
+	 */
 	void set(int column, int row, int value) {
-		this.a[row-1][column-1] = value;
-		int cellnumber = this.scellnumer = scellnumer;
+		int oldvalue, sumoldvalue;
+		oldvalue = this.get(column, row);
+		this.a[row - 1][column - 1] = value;
 		int size = this.sumcells.size();
 		int[] e = new int[0];
-		if (cellnumber > 0) {
-			//System.out.println(size);
-			for (int i = 0; i <= size - 1; i++) {
-				e = this.sumcells.get(i);
-				//System.out.println(Arrays.toString(e));
-				if (e[3] <= row && e[5] >= row && e[2] <= column && column <= e[4]) {
-					this.setSumCalculation(e[0], e[1], e[2], e[3], e[4], e[5]);
-				}
+		for (int i = 0; i <= size - 1; i++) {
+			e = this.sumcells.get(i);
+			if (e[3] <= row && e[5] >= row && e[2] <= column && column <= e[4]) {
+				sumoldvalue = this.get(e[0], e[1]);
+				this.set(e[0], e[1], sumoldvalue - oldvalue + value);
 			}
 		}
-
 	}
 
 	int get(int column, int row) {
-		// System.out.println(this.a[row][column]);
-		return this.a[row-1][column-1];
+		return this.a[row - 1][column - 1];
 	}
 
 	void setSumCalculation(int resultColumn, int resultRow, int startColumn, int startRow, int endColumn, int endRow) {
 		int sum = 0;
-
 		for (int i = startRow; i <= endRow; i++) {
 			for (int j = startColumn; j <= endColumn; j++) {
 				sum = sum + get(j, i);
 			}
 		}
-		 this.set(resultColumn, resultRow, sum);
-	//	this.a[resultRow-1][resultColumn-1] = sum;
-		this.scellnumer = this.scellnumer + 1;
+		this.set(resultColumn, resultRow, sum);
+		//add sum cell into arraylist
 		int[] cellvalues = new int[6];
 		cellvalues[0] = resultColumn;
 		cellvalues[1] = resultRow;
@@ -57,13 +58,7 @@ public class Spreadsheet {
 		cellvalues[3] = startRow;
 		cellvalues[4] = endColumn;
 		cellvalues[5] = endRow;
-		// System.out.println(this.scellnumer);
 		add(cellvalues);
-
-	//	System.out.println(this.sumcells.size());
-		// for(int i = 0; i <= this.sumcells.size()-1; i++) {
-		// System.out.print(Arrays.toString(this.sumcells.get(i)));
-		// }
 
 	}
 
