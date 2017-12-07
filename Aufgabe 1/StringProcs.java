427c85746f8c33523e9bbea58f849bb209fd4ab5
Unknown <mohamad222222222@gmail.com>
2017-12-07 20:40:58 +0100

    1.a. vereinfacht

--- Aufgabe 1/StringProcs.java
+++ Aufgabe 1/StringProcs.java
@@ -1,30 +1,14 @@
 package homework;
 
 public class StringProcs {
-
-
-public static String extension(String Name){
-	 char [] a=Name.toCharArray();
-	 int ḱ=0;
-	 for(int i=0;i<a.length;i++){
-		 if(a[i]=='.'){
-			  ḱ=i;
-			  break;
-			 
-		 }
-		 else{
-			 return "";
-		 }
-	 }
-	 char[] b=new char[a.length-ḱ];
-	 for (int i=0;i<b.length-1;i++){
-		 ḱ++;
-		 b[i]=a[ḱ];
-		 
-	 }
-	 String s=new String(b);
-	 return s;
- }
+	public static String extension(String f) {
+		int punktpos;
+		punktpos = f.lastIndexOf(".");
+		if (punktpos == -1) {
+			return "";
+		}
+		return f.substring(punktpos + 1, f.length());
+	}
 
 public static String SwitchCommaAndDot(String Name){
 	char [] a=Name.toCharArray();
